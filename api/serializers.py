1eef3f799cd102822b4f200c30b84205c483c016
daviddzxy <david.dzxy@gmail.com>
2021-06-30 19:01:41 +0200

    Add serializers.

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import Vaccine, Region, VaccinationReport
+from .models import Vaccine, Region, VaccinationReport, District, AgTestsReport, City, Hospital, HospitalStaffReport,\
+    BedsReport, PatientsReport
 
 
 class VaccineSerializer(serializers.ModelSerializer):
@@ -13,6 +14,47 @@ class RegionSerializer(serializers.ModelSerializer):
         model = Region
         fields = '__all__'
 
+
+class DistrictSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = District
+        fields = '__all__'
+
+
+class AgTestsReportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AgTestsReport
+        fields = '__all__'
+
+
+class CitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = City
+        fields = '__all__'
+
+
+class HospitalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Hospital
+        fields = '__all__'
+
+
+class HospitalStaffReportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = HospitalStaffReport
+        fields = '__all__'
+
+
+class BedsReportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BedsReport
+        fields = '__all__'
+
+
+class PatientsReportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PatientsReport
+        fields = '__all__'
 
 class VaccinationReportSerializer(serializers.ModelSerializer):
     class Meta:
