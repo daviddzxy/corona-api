ca6a9341e93f8d135cc2173c35ea57b65f8e8898
daviddzxy <david.dzxy@gmail.com>
2021-07-06 00:19:48 +0200

    Add urls for report APIs.

--- corona_api/api/urls.py
+++ corona_api/api/urls.py
@@ -7,6 +7,11 @@ urlpatterns = [
     path('hospitals/', views.HospitalListView.as_view()),
     path('districts/', views.DistrictListView.as_view()),
     path('cities/', views.CityListView.as_view()),
+    path('agtestreports/', views.AgTestsReportView.as_view()),
+    path('bedsreports/', views.BedsReportView.as_view()),
+    path('hospitalstaffreports/', views.HospitalStaffReportView.as_view()),
+    path('patientsreports/', views.PatientsReportView.as_view()),
+    path('vaccinationreports/', views.VaccineVaccinationReport.as_view()),
     path('vaccines/<int:pk>/vaccination-reports', views.VaccineVaccinationReport.as_view())
 
 ]
