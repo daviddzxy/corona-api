52c553fe297808cf756d8e449baa76313c0463a3
daviddzxy <david.dzxy@gmail.com>
2021-07-17 17:27:11 +0200

    Add production settings.

--- corona_api/corona_api/settings/production.py
+++ corona_api/corona_api/settings/production.py
@@ -1,6 +1,8 @@
 import os
+import dj_database_url
 from .common import *
 
 DEBUG = False
 SECRET_KEY = os.environ['SECRET_KEY']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
